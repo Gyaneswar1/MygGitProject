2d908496692d59fd1396850f84d29e9c9b59e7bb
Gyaneswar <b.gyaneswar91@gmail.com>
2018-04-19 16:56:32 +0530

    update test script

--- org.majento.project/src/org/majento/tests/Script3.java
+++ org.majento.project/src/org/majento/tests/Script3.java
@@ -10,18 +10,15 @@ import org.majento.pom.MajentoElementOrders;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 
-public class Script3 extends Base {
+public class Script3 extends majentoOrder{
 	@Test
 public void verifyreturnreceived() throws InterruptedException {
      
 	startbrowser("firefox","https://amazon.in");
-	MajentoLogin login=new MajentoLogin();
-	login.logintomajento("b.gyaneswar91@gmail.com", "gyana@7682968092");
-		MajentoHome h=new MajentoHome();
-		h.clickorder();
-	majentoOrder m=new majentoOrder();
-	m.clickviewreturnrefundstatus();
-	String ActualResult=m.gettextpfreturnreceivedprocessingrefund();
+	logintomajento("b.gyaneswar91@gmail.com", "gyana@7682968092");
+		clickorder();
+	clickviewreturnrefundstatus();
+	String ActualResult=gettextpfreturnreceivedprocessingrefund();
 	String exceptedResult="Return received. Processing your refund.";
 assertEquals(exceptedResult,ActualResult );
 System.out.println("the test script is pass"+ActualResult);
