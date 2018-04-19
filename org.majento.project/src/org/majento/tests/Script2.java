eccfd0e957d1f439a9df487d96414f8b0fd0e7cf
Gyaneswar <b.gyaneswar91@gmail.com>
2018-04-19 18:24:32 +0530

    update project

--- org.majento.project/src/org/majento/tests/Script2.java
+++ org.majento.project/src/org/majento/tests/Script2.java
@@ -1,5 +1,7 @@
 package org.majento.tests;
 
+import static org.testng.Assert.assertEquals;
+
 import org.majento.basetest.MajentoHome;
 import org.majento.pom.MajentoElementHome;
 import org.openqa.selenium.support.PageFactory;
@@ -13,14 +15,9 @@ public class Script2 extends MajentoHome{
 		startbrowser("firefox","https://amazon.in");
 		logintomajento("b.gyaneswar91@gmail.com", "gyana@7682968092");
 		clickorder();
-		String expected="Your Orders";
+		String excepted="Your Orders";
 		String actual=gettextofyoursorder();
-		if(expected.equals(actual)) {
-			System.out.println("textscript is pass"+actual);
-		}else {
-			System.out.println("textscript is fail");
-		}
-		driver.close();
+		assertEquals(excepted,actual );	
+		System.out.println("test script is pass"+actual);
 	}
-	
 }
