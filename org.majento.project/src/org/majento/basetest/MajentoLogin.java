f57c9a3ed4f1c3ffd27c808e089e5d0c7cd78906
Gyaneswar <b.gyaneswar91@gmail.com>
2018-04-23 00:40:58 +0530

    update my project

--- org.majento.project/src/org/majento/basetest/MajentoLogin.java
+++ org.majento.project/src/org/majento/basetest/MajentoLogin.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.support.PageFactory;
 import com.beust.jcommander.Parameter;
 
 public class MajentoLogin extends Base {
+private static final String String = null;
 public void logintomajento(String username,String password) {
 	MajentoElementsLogin majentopageref=PageFactory.initElements(driver, MajentoElementsLogin.class);
 	majentopageref.loginbtn.click();
@@ -14,6 +15,15 @@ public void logintomajento(String username,String password) {
 	majentopageref.clickconti.click();
 	majentopageref.pswtextbox.sendKeys(password);
 	majentopageref.clicklogin.click();
+	
+}
+public String invalidpasswordtext() {
+	MajentoElementsLogin majentopageref=PageFactory.initElements(driver, MajentoElementsLogin.class);
+	majentopageref.invalidpswtext.getText();
+	return String;
+	
+}
+public void m1() {
 	
 }
 }
